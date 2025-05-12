6e684240e4ac5da1c261210b5d4d398ea8062dc1
TCORE\KakaraparthiS <Sunil.Kakaraparthi@transcore.com>
2025-05-11 22:03:14 -0400

    Updates

---
+++ MicroRabbit/MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        IEnumerable<Account> GetAccounts();
+        void Transfer(AccountTransfer accountTransfer);
+    }
+}
--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,12 @@
 ﻿//using MediatR;
 //using MicroRabbit.Banking.Application.Interfaces;
 //using MicroRabbit.Banking.Application.Services;
+using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +19,13 @@ namespace MicroRabbit.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
