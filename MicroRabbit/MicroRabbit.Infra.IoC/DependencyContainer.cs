6e38dcca6c5eab4f717eaf4436f0c62c74258f2b
TCORE\KakaraparthiS <Sunil.Kakaraparthi@transcore.com>
2025-05-11 21:26:49 -0400

    Infrac IoC Initial

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿//using MediatR;
+//using MicroRabbit.Banking.Application.Interfaces;
+//using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
